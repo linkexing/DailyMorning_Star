6cf626bb8cb56e8043809e19fae8015f16dc7d93
linkexing <81944534+linkexing@users.noreply.github.com>
2024-04-15 17:06:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -222,9 +222,9 @@ if __name__ == "__main__":
     note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-         if users[0]=="oXtc26eGl1aPNsVuCbWIiycGVNAs":
+        if users[0]=="oXtc26eGl1aPNsVuCbWIiycGVNAs":
             weather, max_temperature, min_temperature = get_weather(province, city)
-         else:
+        else:
             weather, max_temperature, min_temperature = get_weather(province2, city2)
-     send_message(user, accessToken, city, weather, max_temperature, min_temperature, note_ch, note_en)
+       send_message(user, accessToken, city, weather, max_temperature, min_temperature, note_ch, note_en)
     os.system("pause")
